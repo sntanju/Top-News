e60e8e612fc241d0ecab872aadd691bdc836df81
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2025-02-18 10:37:37 +0600

    some design changed

--- app/newses/[id]/page.js
+++ app/newses/[id]/page.js
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation"; // Using useParams
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
 
 async function fetchNewsItem(id) {
   const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
@@ -45,9 +47,11 @@ export default function NewsDetails() {
   if (!news) return <p className="text-center">Loading...</p>;
 
   return (
+    <>
+    <Navbar/>
     <div className="max-w-5xl mx-auto p-4">
       {/* News Card */}
-      <div className="border rounded-lg shadow-lg bg-white p-4">
+      <div className="border  shadow-lg bg-white p-4">
         <div className="flex items-start gap-4">
           {/* Score (Added back) */}
           <div className="w-10 text-right font-bold text-orange-600 mt-5">{news.score}</div>
@@ -82,6 +86,8 @@ export default function NewsDetails() {
         )}
       </div>
     </div>
+    <Footer/>
+    </>
   );
 }
 
@@ -104,12 +110,16 @@ function CommentItem({ comment }) {
 
   return (
     <div className=" pl-4 mt-3">
-      <p className="text-sm text-gray-600">
-        <Link href={`/user/${comment.by}`} className="underline text-orange-500">
-          {comment.by}
-        </Link>{" "}
-        | {timeAgo(comment.time)}
-      </p>
+     <p className="text-sm text-gray-600">
+      <Link href={`/user/${comment.by}`} className="underline text-orange-500">
+        {comment.by}
+      </Link>{" "}
+      | {timeAgo(comment.time)}
+      {comment.kids && comment.kids.length > 0 && (
+        <span> | {comment.kids.length} replies</span>
+      )}
+    </p>
+
       <div className="mt-1 text-gray-900" dangerouslySetInnerHTML={{ __html: comment.text }} />
 
       {comment.kids && comment.kids.length > 0 && (
